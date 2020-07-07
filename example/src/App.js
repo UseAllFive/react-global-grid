ad68f4545496edbae2553314f8ffabca1a7f2705
bretmorris <bret@useallfive.com>
2020-07-06 18:09:11 -0600

    Update example for gh-pages

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { Grid, Column } from 'react-global-grid'
 import { theme } from './theme.js'
+import { gg } from 'css-grid-guides'
+import 'react-global-grid/dist/index.css'
 
 const App = () => {
+  useEffect(() => {
+    gg()
+  }, [])
   return (
     <ThemeProvider theme={theme}>
-      <Grid>
-        <Column span='1 / 4'>column 1</Column>
-        <Column span='5 / -1'>column 2</Column>
+      <Grid className='grid'>
+        <Column sx={{ bg: 'black', color: 'white', height: 200 }} span='1 / 4'>
+          column 1
+        </Column>
+        <Column sx={{ bg: 'green', color: 'white', height: 200 }} span='5 / 7'>
+          column 2
+        </Column>
+        <Column sx={{ bg: 'blue', color: 'white', height: 200 }} span='8 / -1'>
+          column 3
+        </Column>
       </Grid>
     </ThemeProvider>
   )
